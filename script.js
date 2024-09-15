39e91a88756098e341d356b5a69c1a57da662b99
Krikoset <krikosetc@gmail.com>
2024-09-15 22:21:48 +0200

    tests

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ document.addEventListener('mousemove', (event) => {
     const { clientX: x, clientY: y } = event;
     const { innerWidth: width, innerHeight: height } = window;
 
-    const moveX = ((x / width) - 0.5) * 20; // Adjust intensity
-    const moveY = ((y / height) - 0.9) * -20; // Adjust intensity
+    const moveX = ((x / width) - 0.5) * 40; // Increased intensity
+    const moveY = ((y / height) - 0.5) * -40; // Increased intensity
 
     image.style.transform = `rotateY(${moveX}deg) rotateX(${moveY}deg)`;
 });
